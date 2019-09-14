c55a3c710b541a39e91058120924cbe71d983807
Connor Meth <connormeth27@gmail.com>
2019-09-14 17:03:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+  
+  data.each do |prop, hash|
+    hash.each do |attributes, array|
+      array.each do |name|
+        if !pigeon_hash.has_key?(name)
+          pigeon_hash[name] = {}
+        end
+        
+        if !pigeon_hash[name].has_key?(prop)
+          pigeon_hash[name][prop] = []
+        end 
+        
+        if !pigeon_hash[name][prop].include?(attributes)
+          pigeon_hash[name][prop] << attributes.to_s 
+        end
+      end
+    end
+  end
+  pigeon_hash
 end
